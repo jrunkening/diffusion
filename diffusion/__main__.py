079791046f0534fe2e6832c3a78aa3762a4e84ff
JingYu Ning <115336923+jrunkening@users.noreply.github.com>
2023-07-10 17:53:59 +0800

    Revert back to CNN

--- diffusion/__main__.py
+++ diffusion/__main__.py
@@ -14,14 +14,14 @@ MODEL_PATH = Path(__file__).parent.joinpath("../model")
 def train_loop(dataloader, model: Model, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
 
-    model.operator.train()
+    model.train()
     for batch, (xs, _) in enumerate(dataloader):
         # add noise
-        xs, ys = model.preprocess(xs)
+        xs, ts, ys = model.preprocess(xs)
 
         # calculate loss
-        xs, ys = xs.to(device), ys.to(device)
-        loss = loss_fn(model.operator(xs), ys)
+        xs, ts, ys = xs.to(device), ts.to(device), ys.to(device)
+        loss = loss_fn(model(xs, ts), ys)
 
         # back propagation
         loss.backward()
@@ -37,16 +37,16 @@ def train_loop(dataloader, model: Model, loss_fn, optimizer, device):
 def test_loop(dataloader, model: Model, loss_fn, device):
     num_batches = len(dataloader)
 
-    model.operator.eval()
+    model.eval()
     test_loss = 0
     with torch.no_grad():
         for xs, _ in dataloader:
             # add noise
-            xs, ys = model.preprocess(xs)
+            xs, ts, ys = model.preprocess(xs)
 
             # calculate loss
-            xs, ys = xs.to(device), ys.to(device)
-            test_loss += loss_fn(model.operator(xs).detach(), ys).item()
+            xs, ts, ys = xs.to(device), ts.to(device), ys.to(device)
+            test_loss += loss_fn(model(xs, ts).detach(), ys).item()
 
     test_loss /= num_batches
     print(f"Test Error: \n Avg loss: {test_loss:>8f} \n")
@@ -74,21 +74,21 @@ def main(on_gpu=True):
     test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 
     model = Model()
-    # model.operator.load_state_dict(torch.load(MODEL_PATH.joinpath(os.listdir(MODEL_PATH, )[-1])))
+    # model.load_state_dict(torch.load(MODEL_PATH.joinpath(os.listdir(MODEL_PATH, )[-1])))
     learning_rate = 1e-4
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.operator.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     epochs = 500
 
     for t in range(epochs):
-        model.operator = model.operator.to(device)
+        model = model.to(device)
 
         print(f"Epoch {t+1}\n-------------------------------")
         train_loop(train_dataloader, model, loss_fn, optimizer, device)
         test_loop(test_dataloader, model, loss_fn, device)
 
-        model.operator = model.operator.to("cpu")
-        torch.save(model.operator.state_dict(), MODEL_PATH.joinpath("model.pth"))
+        model = model.to("cpu")
+        torch.save(model.state_dict(), MODEL_PATH.joinpath("model.pth"))
 
     print("Done!")
 
--- diffusion/model.py
+++ diffusion/model.py
@@ -1,45 +1,115 @@
 import torch
 from neuralop.models import UNO
 from diffusion.noise_scheduler import NoiseScheduler
+from torch import nn
 
 
-class Model:
-    def __init__(self) -> None:
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.register_buffer("embedding_w", torch.randn(256//2))
         self.noise_scheduler = NoiseScheduler(0, 0.02, 1000)
-        self.operator = UNO(
-            in_channels=6,
-            out_channels=3,
-            hidden_channels=128,
-            lifting_channels=256,
-            projection_channels=256,
-            n_layers=4,
-            uno_out_channels=[64, 32, 32, 64],
-            uno_n_modes=[[128, 128], [128, 128], [128, 128], [128, 128]],
-            uno_scalings=[[1, 1], [1, 1], [1, 1], [1, 1]],
-        )
 
-    def cat_pos_info(self, xs, ts):
-        y_pos = torch.ones(xs.shape[0], 1, *xs.shape[2:])
-        x_pos = torch.ones(xs.shape[0], 1, *xs.shape[2:])
-        t_pos = torch.ones(xs.shape[0], 1, *xs.shape[2:])
-        for i in range(xs.shape[2]):
-            y_pos[:, :, i, :] *= i
-        for i in range(xs.shape[3]):
-            x_pos[:, :, :, i] *= i
-        for i, t in enumerate(ts):
-            t_pos[i, :, :, :] *= t
+        self.channels = [3, 16, 32, 64, 128]
+        self.act = nn.GELU()
 
-        xs = torch.cat((xs, y_pos), dim=1)
-        xs = torch.cat((xs, x_pos), dim=1)
-        xs = torch.cat((xs, t_pos), dim=1)
+        self.conv1 = nn.Conv2d(self.channels[0], self.channels[1], (3, 3), padding=(1, 1))
+        self.d1 = nn.Linear(256, self.channels[1])
+        self.pool1 = nn.MaxPool2d((2, 2))
+        self.n1 = nn.GroupNorm(16, self.channels[1])
+        self.conv2 = nn.Conv2d(self.channels[1], self.channels[2], (3, 3), padding=(1, 1))
+        self.d2 = nn.Linear(256, self.channels[2])
+        self.pool2 = nn.MaxPool2d((2, 2), stride=(1, 1))
+        self.n2 = nn.GroupNorm(32, self.channels[2])
+        self.conv3 = nn.Conv2d(self.channels[2], self.channels[3], (3, 3), padding=(1, 1))
+        self.d3 = nn.Linear(256, self.channels[3])
+        self.pool3 = nn.MaxPool2d((2, 2))
+        self.n3 = nn.GroupNorm(32, self.channels[3])
+        self.conv4 = nn.Conv2d(self.channels[3], self.channels[4], (3, 3), padding=(1, 1))
+        self.d4 = nn.Linear(256, self.channels[4])
+        self.pool4 = nn.MaxPool2d((2, 2))
+        self.n4 = nn.GroupNorm(32, self.channels[4])
 
-        return xs
+        self.up4 = nn.ConvTranspose2d(self.channels[4], self.channels[3], (2, 2), stride=(2, 2))
+        self.dt4 = nn.Linear(256, self.channels[3])
+        self.convt4 = nn.ConvTranspose2d(self.channels[3], self.channels[3], (3, 3), padding=(1, 1))
+        self.nt4 = nn.GroupNorm(32, self.channels[3])
+        self.up3 = nn.ConvTranspose2d(2*self.channels[3], self.channels[2], (2, 2), stride=(2, 2))
+        self.dt3 = nn.Linear(256, self.channels[2])
+        self.convt3 = nn.ConvTranspose2d(self.channels[2], self.channels[2], (3, 3), padding=(1, 1))
+        self.nt3 = nn.GroupNorm(32, self.channels[2])
+        self.up2 = nn.ConvTranspose2d(2*self.channels[2], self.channels[1], (2, 2), stride=(1, 1))
+        self.dt2 = nn.Linear(256, self.channels[1])
+        self.convt2 = nn.ConvTranspose2d(self.channels[1], self.channels[1], (3, 3), padding=(1, 1))
+        self.nt2 = nn.GroupNorm(16, self.channels[1])
+        self.up1 = nn.ConvTranspose2d(2*self.channels[1], self.channels[0], (2, 2), stride=(2, 2))
+        self.dt1 = nn.Linear(256, self.channels[0])
+        self.convt1 = nn.ConvTranspose2d(self.channels[0], self.channels[0], (3, 3), padding=(1, 1))
+        self.nt1 = nn.GroupNorm(3, self.channels[0])
 
     def preprocess(self, xs):
         ts = torch.randint(1, self.noise_scheduler.steps, (len(xs),))
         xs, ys = self.noise_scheduler.forward_process(xs, ts)
 
-        return self.cat_pos_info(xs, ts), ys
+        return xs, ts, ys
+
+    def embed(self, ts, linear):
+        embedding = 30 * torch.outer(ts, self.embedding_w)
+        embedding = torch.cat([torch.sin(embedding), torch.cos(embedding)], dim=1)
+        embedding = self.act(linear(embedding))
+        embedding = embedding.reshape(*embedding.shape, 1, 1)
+
+        return embedding
+
+    def forward(self, xs, ts):
+        h1 = self.conv1(xs)
+        h1 += self.embed(ts, self.d1)
+        h1 = self.pool1(h1)
+        h1 = self.n1(h1)
+        h1 = self.act(h1)
+
+        h2 = self.conv2(h1)
+        h2 += self.embed(ts, self.d2)
+        h2 = self.pool2(h2)
+        h2 = self.n2(h2)
+        h2 = self.act(h2)
+
+        h3 = self.conv3(h2)
+        h3 += self.embed(ts, self.d3)
+        h3 = self.pool3(h3)
+        h3 = self.n3(h3)
+        h3 = self.act(h3)
+
+        h4 = self.conv4(h3)
+        h4 += self.embed(ts, self.d4)
+        h4 = self.pool4(h4)
+        h4 = self.n4(h4)
+        h4 = self.act(h4)
+
+
+        h = self.up4(h4)
+        h += self.embed(ts, self.dt4)
+        h = self.convt4(h)
+        h = self.nt4(h)
+        h = self.act(h)
+
+        h = self.up3(torch.cat((h, h3), dim=1))
+        h += self.embed(ts, self.dt3)
+        h = self.convt3(h)
+        h = self.nt3(h)
+        h = self.act(h)
+
+        h = self.up2(torch.cat((h, h2), dim=1))
+        h += self.embed(ts, self.dt2)
+        h = self.convt2(h)
+        h = self.nt2(h)
+        h = self.act(h)
+
+        h = self.up1(torch.cat((h, h1), dim=1))
+        h = self.convt1(h)
+        h = self.nt1(h)
+
+        return h
 
     def prev(self, image, t):
         t = torch.tensor(t).reshape(1)
@@ -49,15 +119,15 @@ class Model:
         sigma = self.noise_scheduler.sigmas[t]
 
         return (
-            image - ((1 - alpha) / sqrt_oneminus_alpha_bar) * self.operator(self.cat_pos_info(image, t)).detach()
+            image - ((1 - alpha) / sqrt_oneminus_alpha_bar) * self(image, t).detach()
         ) / torch.sqrt(alpha) + sigma * torch.randn_like(image)
 
     def infer(self, image_dims):
         image = torch.randn(1, 3, *image_dims)
-        for i in range(self.noise_scheduler.steps-1, -1, -1):
+        for i in range(self.noise_scheduler.steps-1, 0, -1):
             image = self.prev(image, i)
         image = image.reshape(3, *image_dims).permute(1, 2, 0)
-        # image -= torch.min(image)
-        # image /= torch.max(image)
+        image -= torch.min(image)
+        image /= torch.max(image)
 
         return image
