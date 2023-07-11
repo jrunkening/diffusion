e6b10272df735fd3db6ed6615a9ae699fbd992fd
JingYu Ning <115336923+jrunkening@users.noreply.github.com>
2023-07-12 00:46:43 +0800

    Refactor

--- script/download_dataset.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import torchvision.datasets as datasets
-
-
-DATA_PATH = Path(__file__).parent.joinpath("../data")
-
-celeba = datasets.CelebA(DATA_PATH, "all", download=True)
